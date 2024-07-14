f301aa552423cfcedd60741145db8e657d6b41fb
rfilinge <56085236+rfilinge@users.noreply.github.com>
2024-07-14 19:49:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('HistoricalStandings.csv')
+    fetch('CompleteData/HistoricalStandings.csv')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
