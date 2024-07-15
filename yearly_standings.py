9eecce8b25e8f354dfc49b9b7a1630b46c3a92c4
rfilinge <56085236+rfilinge@users.noreply.github.com>
2024-07-15 01:32:22 -0400

    added ids

--- yearly_standings.py
+++ yearly_standings.py
@@ -6,43 +6,52 @@ import datetime
 firstSeason = 2023
 currentSeason = 2024
 
+for x in range(currentSeason-firstSeason):
+    currYear = firstSeason + x
+
+    df_matchups = pd.read_csv('RawData/DirectMatchups.csv')
+    df_teams = pd.read_csv('RawData/TeamNames.csv')
+
+    df_standings = pd.DataFrame(columns=['teamId','Team', 'W', 'L', 'Points For', 'Points Against', 'Average PF', 'Average PA'])
+
+    df_currYear_matchups = df_matchups.loc[df_matchups['Season'] == currYear]
+    df_currYear_Teams = df_teams.loc[df_matchups['Season'] == currYear]
+    
+
+    def to_decimal(val):
+        return Decimal(str(val)) if isinstance(val, float) else val
+
+    for col in df_currYear_matchups.columns:
+        if col != 'name':
+            df_currYear_matchups[col] = df_currYear_matchups[col].apply(to_decimal)
+
+    for x in range(len(df_currYear_Teams)):
+        teamName = df_currYear_Teams.iloc[x]['Name']
+        teamId = df_currYear_Teams.iloc[x]['Id']
+        df_wins = df_matchups.loc[df_matchups['Winner'] ==teamName]
+        df_losses = df_matchups.loc[df_matchups['Loser'] == teamName]
+
+        wins = len(df_wins)
+        losses = len(df_losses)
+
+        pointsFor = df_wins['wScore'].sum() + df_losses['lScore'].sum()
+        pointsAgainst = df_wins['lScore'].sum() + df_losses['wScore'].sum()
+        print(pointsFor)
+        print(pointsAgainst)
+
+        averagePf = round(pointsFor/(wins + losses), 2)
+        averagePa = round(pointsAgainst/(wins + losses), 2)
 
 
-df_matchups = pd.read_csv('RawData/DirectMatchups.csv')
-df_teams = pd.read_csv('RawData/TeamNames.csv')
 
-df_Terps = len(df_matchups.loc[df_matchups['Winner'] =='Dirty Terps'])
-df_standings = pd.DataFrame(columns=['Team', 'W', 'L', 'Points For', 'Points Against', 'Average PF', 'Average PA'])
-team_records = []
+        df_standings.loc[len(df_standings)] = [teamId, teamName, wins, losses, pointsFor, pointsAgainst, averagePf, averagePa]
 
-def to_decimal(val):
-    return Decimal(str(val)) if isinstance(val, float) else val
 
-for col in df_matchups.columns:
-    if col != 'name':
-        df_matchups[col] = df_matchups[col].apply(to_decimal)
-
-for x in range(len(df_teams)):
-    teamName = df_teams.iloc[x]['Name']
-    df_wins = df_matchups.loc[df_matchups['Winner'] ==teamName]
-    df_losses = df_matchups.loc[df_matchups['Loser'] == teamName]
-
-    wins = len(df_wins)
-    losses = len(df_losses)
-
-    pointsFor = df_wins['wScore'].sum() + df_losses['lScore'].sum()
-    pointsAgainst = df_wins['lScore'].sum() + df_losses['wScore'].sum()
-    print(pointsFor)
-    print(pointsAgainst)
-
-    averagePf = round(pointsFor/(wins + losses), 2)
-    averagePa = round(pointsAgainst/(wins + losses), 2)
+        print(df_standings)
+        df_standings.to_csv('CompleteData/Historical/HistoricalStandings.csv', index=False)
 
 
 
-    df_standings.loc[len(df_standings)] = [teamName, wins, losses, pointsFor, pointsAgainst, averagePf, averagePa]
 
 
-print(df_standings)
-df_standings.to_csv('CompleteData/Historical/HistoricalStandings.csv', index=False)
 
