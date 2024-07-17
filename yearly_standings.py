dbedf61f3b58eb65f48a9a907567b270be0e73ea
rfilinge <56085236+rfilinge@users.noreply.github.com>
2024-07-17 03:06:01 -0400

    added place

--- yearly_standings.py
+++ yearly_standings.py
@@ -12,7 +12,7 @@ for x in range(currentSeason-firstSeason):
     df_matchups = pd.read_csv('RawData/DirectMatchups.csv')
     df_teams = pd.read_csv('RawData/TeamNames.csv')
 
-    df_standings = pd.DataFrame(columns=['teamId','Team', 'W', 'L', 'Points For', 'Points Against', 'Average PF', 'Average PA'])
+    df_standings = pd.DataFrame(columns=['Place','teamId','Team', 'W', 'L', 'Points For', 'Points Against', 'Average PF', 'Average PA'])
 
     df_currYear_matchups = df_matchups.loc[df_matchups['Season'] == currYear]
     df_currYear_Teams = df_teams.loc[df_teams['Season'] == currYear]
@@ -27,6 +27,7 @@ for x in range(currentSeason-firstSeason):
     for x in range(len(df_currYear_Teams)):
         teamName = df_currYear_Teams.iloc[x]['Name']
         teamId = df_currYear_Teams.iloc[x]['Id']
+        regFin = df_currYear_Teams.iloc[x]['RegSeasonFin']
         df_wins = df_currYear_matchups.loc[df_currYear_matchups['Winner'] ==teamName]
         df_losses = df_currYear_matchups.loc[df_currYear_matchups['Loser'] == teamName]
 
@@ -41,7 +42,7 @@ for x in range(currentSeason-firstSeason):
         averagePf = round(pointsFor/(wins + losses), 2)
         averagePa = round(pointsAgainst/(wins + losses), 2)
 
-        df_standings.loc[len(df_standings)] = [teamId, teamName, wins, losses, pointsFor, pointsAgainst, averagePf, averagePa]
+        df_standings.loc[len(df_standings)] = [regFin, teamId, teamName, wins, losses, pointsFor, pointsAgainst, averagePf, averagePa]
 
         print(df_standings)
         df_standings.to_csv('CompleteData/' + str(currYear) + '/' + str(currYear) + 'Standings.csv', index=False)
